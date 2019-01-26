6fee90fa9bbed336732b6ffc64fc5d3c99d93acc
Alex Kashuba <o.kashuba@gmail.com>
2019-01-27 00:04:55 +0100

    Processing and search for _abrv.dsl and .ann (not finished)
    --annotation FILE sets annotation= FILE
    --no-annotation sets annotation=False
    if both absent, annotation=None

--- lexicamaker/__main__.py
+++ lexicamaker/__main__.py
@@ -17,11 +17,12 @@ def arguments_parser():
     parser.add_argument('-v', '--verbose', action='count', help="increase output verbosity")
     group = parser.add_argument_group('Fine tuning')
     subgroup1 = group.add_mutually_exclusive_group()
-    subgroup1.add_argument('--no-annotation', action='store_false', help="Ignore annotation")
+    subgroup1.add_argument('--no-annotation', dest='annotation', action='store_false', help="Ignore annotation")
     #subgroup1.add_argument('--annotation', metavar='FILE', type=argparse.FileType('r', encoding='utf-16le'), help="annotation file")
     subgroup1.add_argument('--annotation', metavar='FILE', help="annotation file")
     #group.add_argument('--abbreviations', metavar='FILE', type=argparse.FileType('r', encoding='utf-16le'), help="abbreviations file")
     group.add_argument('--abbreviations', metavar='FILE', help="abbreviations file")
+    group.add_argument('--name', metavar='NAME', help="set dictionary name")
     subgroup2 = group.add_mutually_exclusive_group()
     subgroup2.add_argument('--no-media', action='store_true', help="Skip media")
     subgroup2.add_argument('--media', metavar='TYPE', type=str, help="Change media type to TYPE (supported wav, mp3, m4a)")
@@ -33,18 +34,32 @@ def main():
     args = arguments_parser().parse_args()
     #   print (args)
 
-    outputDictionaryPath, dictionaryExtension = os.path.splitext(args.dslfile)
-
+    dictionaryPath = os.path.dirname(args.dslfile)
+    dictionaryName, dictionaryExtension = os.path.splitext(os.path.basename(args.dslfile))
+    
     if dictionaryExtension.lower() != '.dsl':
         sys.stderr.write ("Warning: dictionary file does not have a .dsl extension.\n")
-        outputDictionaryPath = args.dslfile
+        dictionaryName += dictionaryExtension
     
-    dictionaryName = os.path.basename(outputDictionaryPath)
-    
+    if args.name != None:
+        dictionaryName = args.name
+
     if args.outdir != None:
         outputDictionaryPath = os.path.join(args.outdir, dictionaryName)
+    else:
+        outputDictionaryPath = os.path.join(dictionaryPath, dictionaryName)
 
     dictionaryFile = open(args.dslfile, 'r', encoding='utf-16le')
+    if args.annotation != None:
+        annotationFile = open(args.annotation, 'r', encoding='utf-16le')
+    else:
+        annotationFile = open(os.path.join(dictionaryPath, dictionaryName) + '.ann', 'r', encoding='utf-16le')
+
+    if args.abbreviations != None:
+        abbreviationsFile = open(args.abbreviations, 'r', encoding='utf-16le')
+    else:
+        abbreviationsFile = open(os.path.join(dictionaryPath, dictionaryName) + '_abrv.dsl', 'r', encoding='utf-16le')
+
 
     print (dictionaryFile)
     print (outputDictionaryPath)
