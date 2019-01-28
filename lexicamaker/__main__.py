18399e491fbb6e32e93b499efd1e1173bf95c190
Alex Kashuba <o.kashuba@gmail.com>
2019-01-28 15:57:58 +0100

    Object-oriented data storing
    IOBridge object
    parse procedures are methods
    tests fixed

--- lexicamaker/__main__.py
+++ lexicamaker/__main__.py
@@ -6,64 +6,96 @@ import os, sys
 import argparse
 from lexicamaker import __version__
 
-def arguments_parser():
-    parser = argparse.ArgumentParser(prog='adsmaker',
-                                     description="Lexicamaker v%s - creates Apple Dictionary Service folder from DSL dictionary" % __version__,
-                                     #usage='%(prog)s [options] DSL_FILE [OUTPUT_DIR]\n       %(prog)s [--help | --version]',
-                                     epilog="The created Apple Dictionary Service folder must be then compiled with Apple Dictionary Development Kit")
-    #parser.add_argument('dslfile', metavar='DSL_FILE', type=argparse.FileType('r', encoding='utf-16le'), help="a DSL dictionary file")
-    parser.add_argument('dslfile', metavar='DSL_FILE', help="a DSL dictionary file")
-    parser.add_argument('outdir', metavar='OUTPUT_DIR', nargs='?', help="a path for Apple Dictionary Service folder")
-    parser.add_argument('-v', '--verbose', action='count', help="increase output verbosity")
-    group = parser.add_argument_group('Fine tuning')
-    subgroup1 = group.add_mutually_exclusive_group()
-    subgroup1.add_argument('--no-annotation', dest='annotation', action='store_false', help="Ignore annotation")
-    #subgroup1.add_argument('--annotation', metavar='FILE', type=argparse.FileType('r', encoding='utf-16le'), help="annotation file")
-    subgroup1.add_argument('--annotation', metavar='FILE', help="annotation file")
-    #group.add_argument('--abbreviations', metavar='FILE', type=argparse.FileType('r', encoding='utf-16le'), help="abbreviations file")
-    group.add_argument('--abbreviations', metavar='FILE', help="abbreviations file")
-    group.add_argument('--name', metavar='NAME', help="set dictionary name")
-    subgroup2 = group.add_mutually_exclusive_group()
-    subgroup2.add_argument('--no-media', action='store_true', help="Skip media")
-    subgroup2.add_argument('--media', metavar='TYPE', type=str, help="Change media type to TYPE (supported wav, mp3, m4a)")
-    parser.add_argument('--version', action='version', version="lexicamaker v%s" % __version__ ) #"%(prog)s v{}".format(__version__))
-    return parser
+class IOBridge:
+    
+    def __init__(self):
+        
+        self.parser = argparse.ArgumentParser(prog='adsmaker',
+                                         description="Lexicamaker v%s - creates Apple Dictionary Service folder from DSL dictionary" % __version__,
+                                         #usage='%(prog)s [options] DSL_FILE [OUTPUT_DIR]\n       %(prog)s [--help | --version]',
+                                         epilog="The created Apple Dictionary Service folder must be then compiled with Apple Dictionary Development Kit")
+        #self.parser.add_argument('dslfile', metavar='DSL_FILE', type=argparse.FileType('r', encoding='utf-16le'), help="a DSL dictionary file")
+        self.parser.add_argument('dictionaryFile', metavar='DSL_FILE', help="a DSL dictionary file")
+        subgroup0 = self.parser.add_mutually_exclusive_group()
+        subgroup0.add_argument('outputDictionaryPath', metavar='OUTPUT_DIR', nargs='?', default=os.getcwd(), help="a path for Apple Dictionary Service folder (uses current directory by default)")
+        self.parser.add_argument('-v', '--verbose', action='count', help="increase output verbosity")
+        group = self.parser.add_argument_group('Fine tuning')
+        subgroup1 = group.add_mutually_exclusive_group()
+        subgroup1.add_argument('--no-annotation', dest='annotationFile', action='store_false', help="Ignore annotation")
+        #subgroup1.add_argument('--annotation', metavar='FILE', type=argparse.FileType('r', encoding='utf-16le'), help="annotation file")
+        subgroup1.add_argument('--annotation', metavar='FILE', dest='annotationFile', help="annotation file")
+        subgroup2 = group.add_mutually_exclusive_group()
+        #group.add_argument('--abbreviations', metavar='FILE', type=argparse.FileType('r', encoding='utf-16le'), help="abbreviations file")
+        subgroup2.add_argument('--abbreviations', metavar='FILE', dest='abbreviationsFile', help="abbreviations file")
+        subgroup2.add_argument('--no-abbreviations', dest='abbreviationsFile', action='store_false', help="Ignore abbreviations")
+        group.add_argument('--name', metavar='NAME', dest='dictionaryName', help="set dictionary name")
+        subgroup3 = group.add_mutually_exclusive_group()
+        subgroup3.add_argument('--no-media', action='store_true', help="Skip media")
+        subgroup3.add_argument('--media', metavar='TYPE', choices=['wav', 'mp3', 'm4a', 'aac'], help="Change media type to TYPE (supported wav, mp3, m4a, aac)")
+        group.add_argument('--encoding', metavar='ENCODING', default='utf-16le', help="set dictionary name")
+        self.parser.add_argument('--version', action='version', version="lexicamaker v%s" % __version__ ) #"%(prog)s v{}".format(__version__))
+        subgroup0.add_argument('--remote', action='store_true', help="forces to build the Apple Dictionary Service folder next to main DSL_FILE dictionary")
+
+    def parse_args(self):
+        self.parser.parse_args(namespace = self)
+
+    def open_files(self):
+        self.parse_args()
+
+        dictionaryPath = os.path.dirname(self.dictionaryFile)
+        dictionaryName, dictionaryExtension = os.path.splitext(os.path.basename(self.dictionaryFile))
+
+        if dictionaryExtension.lower() != '.dsl':
+            sys.stderr.write ("Warning: dictionary file does not have a .dsl extension.\n")
+            dictionaryName += dictionaryExtension
+    
+        if not self.dictionaryName:
+            self.dictionaryName = dictionaryName
+
+        if self.remote:
+            self.outputDictionaryPath = os.path.join(dictionaryPath, dictionaryName)
+        else:
+            self.outputDictionaryPath = os.path.join(self.outputDictionaryPath, dictionaryName)
+        
+        self.dictionaryFile = open(self.dictionaryFile, 'r', encoding='utf-16le')
+
+        if self.annotationFile :
+            self.annotationFile = open(self.annotationFile, 'r', encoding='utf-16le')
+        elif self.annotationFile == None:
+            try:
+                self.annotationFile = open(os.path.join(dictionaryPath, dictionaryName) + '.ann', 'r', encoding='utf-16le')
+            except FileNotFoundError:
+                self.annotationFile = None
+
+        if self.abbreviationsFile :
+            self.abbreviationsFile = open(self.abbreviationsFile, 'r', encoding='utf-16le')
+        elif self.abbreviationsFile == None:
+            try:
+                self.abbreviationsFile = open(os.path.join(dictionaryPath, dictionaryName) + '_abrv.dsl', 'r', encoding='utf-16le')
+            except FileNotFoundError:
+                self.abbreviationsFile = None
+
+
+
+        print (self.outputDictionaryPath)
+        print (self.dictionaryName)
+        print (self.dictionaryFile)
+        print (self.annotationFile)
+        print (self.abbreviationsFile)
+
 
 def main():
-
-    args = arguments_parser().parse_args()
-    #   print (args)
-
-    dictionaryPath = os.path.dirname(args.dslfile)
-    dictionaryName, dictionaryExtension = os.path.splitext(os.path.basename(args.dslfile))
+    ioDict = IOBridge()
     
-    if dictionaryExtension.lower() != '.dsl':
-        sys.stderr.write ("Warning: dictionary file does not have a .dsl extension.\n")
-        dictionaryName += dictionaryExtension
+    ioDict.open_files()
+    #args = arguments_parser().parse_args()
+
     
-    if args.name != None:
-        dictionaryName = args.name
-
-    if args.outdir != None:
-        outputDictionaryPath = os.path.join(args.outdir, dictionaryName)
-    else:
-        outputDictionaryPath = os.path.join(dictionaryPath, dictionaryName)
-
-    dictionaryFile = open(args.dslfile, 'r', encoding='utf-16le')
-    if args.annotation != None:
-        annotationFile = open(args.annotation, 'r', encoding='utf-16le')
-    else:
-        annotationFile = open(os.path.join(dictionaryPath, dictionaryName) + '.ann', 'r', encoding='utf-16le')
-
-    if args.abbreviations != None:
-        abbreviationsFile = open(args.abbreviations, 'r', encoding='utf-16le')
-    else:
-        abbreviationsFile = open(os.path.join(dictionaryPath, dictionaryName) + '_abrv.dsl', 'r', encoding='utf-16le')
 
 
-    print (dictionaryFile)
-    print (outputDictionaryPath)
-    print (dictionaryName)
+
+
+#    print (dictionaryFile)
 
 
 
--- tests/test_setparams.py
+++ tests/test_setparams.py
@@ -21,15 +21,17 @@ def test_argparse1():
     """Checks import of the parser function and tests the simplest argument configuration.
         1) %(prog) testsource/TestDictionary.dsl
         2) %(prog) testsource/TestDictionary.dsl somedir"""
-    from lexicamaker.__main__ import arguments_parser
+    from lexicamaker.__main__ import IOBridge
     
 
     os.chdir(os.path.dirname(__file__))
-    assert arguments_parser().parse_args(['path/file.dsl']).dslfile == 'path/file.dsl'
-    #assert arguments_parser().parse_args(['path/file.dsl']).outdir == 'path/file'
-    #assert arguments_parser().parse_args(['path/file.dsa']).outdir == 'path/file.dsa'
-    assert arguments_parser().parse_args(['path/file.dsl', 'adir']).outdir == 'adir'
-    #assert arguments_parser().parse_args(['path/file.dsa', 'adir']).outdir == 'adir/file.dsa'
+
+    args = IOBridge()
+    assert args.parser.parse_args(['path/file.dsl']).dictionaryFile == 'path/file.dsl'
+    assert args.parser.parse_args(['path/file.dsl']).outputDictionaryPath == os.getcwd()
+    #assert args.parser.parse_args(['path/file.dsl']).outputDictionaryPath == 'path/file.dsa'
+    assert args.parser.parse_args(['path/file.dsl', 'adir']).outputDictionaryPath == 'adir'
+    #assert args.parser.parse_args(['path/file.dsa', 'adir']).outputDictionaryPath == 'adir/file.dsa'
 
 
 
@@ -37,15 +39,17 @@ def test_argparse2():
     """Checks import of the parser function and tests the simplest argument configuration.
         1) --version
         2) --help"""
-    from lexicamaker.__main__ import arguments_parser
+    from lexicamaker.__main__ import IOBridge
+    
+    args = IOBridge()
     
     try:
-        arguments_parser().parse_args(['--version'])
+        args.parser.parse_args(['--version'])
     except SystemExit as se:
         print("\"--version\": detect raised SystemExit with code %d" % se.code)
         assert se.code==0
     try:
-        arguments_parser().parse_args(['--help'])
+        args.parser.parse_args(['--help'])
     except SystemExit as se:
         print("\"--help\": detect raised SystemExit with code %d" % se.code)
         assert se.code==0
