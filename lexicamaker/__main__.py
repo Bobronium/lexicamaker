ed841ecd167687ebae7b6952ee0059be9b6bd3f0
Alex Kashuba <o.kashuba@gmail.com>
2019-01-30 13:22:38 +0100

    Help, Encoding, Test dictionaries
    Improved the help
    Allowed to use different encoding
    Added three-words test dictionaries. The encoding is UTF-16, with FF FE (little-endian) BOM in the beginning.

--- lexicamaker/__main__.py
+++ lexicamaker/__main__.py
@@ -18,22 +18,32 @@ class IOBridge:
         #parser.add_argument('dictionaryFile', metavar='DSL_FILE', help="a DSL dictionary file")
         parser.add_argument('dictionaryFile', metavar='DSL_FILE', help="a DSL dictionary file")
         subgroup_opath = parser.add_mutually_exclusive_group()
-        subgroup_opath.add_argument('outputDictionaryPath', metavar='OUTPUT_DIR', nargs='?', default=os.getcwd(), help="a path for Apple Dictionary Service folder (uses current directory by default)")
-        parser.add_argument('-v', '--verbose', action='count', default=0, help="increase output verbosity")
+        subgroup_opath.add_argument('outputDictionaryPath', metavar='OUTPUT_DIR', nargs='?', default=os.getcwd(),
+                                    help="Use an OUTPUT_DIR to place Apple Dictionary Service folder (uses current directory by default)")
+        parser.add_argument('-v', '--verbose', action='count', default=0,
+                                    help="Increases output verbosity")
         group = parser.add_argument_group('Fine tuning')
         subgroup_ann = group.add_mutually_exclusive_group()
-        subgroup_ann.add_argument('--annotation', metavar='FILE', dest='annotationFile', default=None, help="annotation file")
-        subgroup_ann.add_argument('--no-annotation', dest='annotationFile', action='store_false', help="ignore annotation")
+        subgroup_ann.add_argument('--annotation', metavar='FILE', dest='annotationFile', default=None,
+                                    help="Use FILE as annotation file")
+        subgroup_ann.add_argument('--no-annotation', dest='annotationFile', action='store_false',
+                                    help="Ignore annotation files")
         subgroup_abrv = group.add_mutually_exclusive_group()
-        subgroup_abrv.add_argument('--abbreviations', metavar='FILE', dest='abbreviationsFile', default=None, help="abbreviations file")
-        subgroup_abrv.add_argument('--no-abbreviations', dest='abbreviationsFile', action='store_false', help="ignore abbreviations")
+        subgroup_abrv.add_argument('--abbreviations', metavar='FILE', dest='abbreviationsFile', default=None,
+                                    help="Use FILE as abbreviations file")
+        subgroup_abrv.add_argument('--no-abbreviations', dest='abbreviationsFile', action='store_false',
+                                    help="Ignore abbreviations files")
         group.add_argument('--name', metavar='NAME', dest='dictionaryName', help="set dictionary name")
         subgroup_media = group.add_mutually_exclusive_group()
-        subgroup_media.add_argument('--no-media', action='store_true', help="Skip media")
-        subgroup_media.add_argument('--media', metavar='TYPE', choices=['wav', 'mp3', 'm4a', 'aac'], help="Change media type to TYPE (supported wav, mp3, m4a, aac)")
-        group.add_argument('--encoding', metavar='ENCODING', default='utf-16le', help="set dictionary name")
+        subgroup_media.add_argument('--no-media', action='store_true', dest='media',
+                                    help="Skip media entries")
+        subgroup_media.add_argument('--media', metavar='TYPE', choices=['wav', 'mp3', 'm4a', 'aac'],
+                                    help="Change media type to TYPE. Supported formats are \'wav\', \'mp3\', \'m4a\', and \'aac\'")
+        group.add_argument('--encoding', metavar='ENCODING', default='utf-16', choices=['utf-8', 'utf-16', 'utf-16le', 'utf-16be'],
+                                    help="Set DSL dictionary encoding, suppored encodings are \'utf-8\' and \'utf-16\' (default). If in latter encoding the Byte Order Mark is is missing use \'utf-16le\' or \'utf-16be\'.")
         parser.add_argument('--version', action='version', version="lexicamaker v%s" % __version__ ) #"%(prog)s v{}".format(__version__))
-        subgroup_opath.add_argument('--remote', action='store_true', help="forces to build the Apple Dictionary Service folder next to main DSL_FILE dictionary")
+        subgroup_opath.add_argument('--remote', action='store_true',
+                                    help="Forces to place the Apple Dictionary Service folder next to main DSL_FILE dictionary")
 
     __set_parser__(parser)
 
@@ -48,7 +58,6 @@ class IOBridge:
 
 
     def open_files(self):
-        #self.parse_args()
 
         dictionaryPath = os.path.dirname(self.dictionaryFile)
         dictionaryName, dictionaryExtension = os.path.splitext(os.path.basename(self.dictionaryFile))
@@ -66,22 +75,22 @@ class IOBridge:
         else:
             self.outputDictionaryPath = os.path.join(self.outputDictionaryPath, self.dictionaryName)
         
-        self.dictionaryFile = open(self.dictionaryFile, 'r', encoding='utf-16le')
+        self.dictionaryFile = open(self.dictionaryFile, 'r', encoding=self.encoding)
 
         if self.annotationFile :
-            self.annotationFile = open(self.annotationFile, 'r', encoding='utf-16le')
+            self.annotationFile = open(self.annotationFile, 'r', encoding=self.encoding)
         elif self.annotationFile is None:
             try:
-                self.annotationFile = open(os.path.join(dictionaryPath, dictionaryName) + '.ann', 'r', encoding='utf-16le')
+                self.annotationFile = open(os.path.join(dictionaryPath, dictionaryName) + '.ann', 'r', encoding=self.encoding)
             except FileNotFoundError:
                 if self.verbose >= 2:
                     sys.stdout.write ("Warning: annotation file is not found.\n")
 
         if self.abbreviationsFile :
-            self.abbreviationsFile = open(self.abbreviationsFile, 'r', encoding='utf-16le')
+            self.abbreviationsFile = open(self.abbreviationsFile, 'r', encoding=self.encoding)
         elif self.abbreviationsFile is None:
             try:
-                self.abbreviationsFile = open(os.path.join(dictionaryPath, dictionaryName) + '_abrv.dsl', 'r', encoding='utf-16le')
+                self.abbreviationsFile = open(os.path.join(dictionaryPath, dictionaryName) + '_abrv.dsl', 'r', encoding=self.encoding)
             except FileNotFoundError:
                 if self.verbose >= 2:
                     sys.stdout.write ("Warning: abbreviations file is not found.\n")
@@ -94,7 +103,7 @@ def main():
     
     ioDict.open_files()
 
-
+    print (ioDict.encoding)
     print (ioDict.outputDictionaryPath)
     print (ioDict.dictionaryName)
     print (ioDict.dictionaryFile.name)
