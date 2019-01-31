cf2e139c8149fa217452157f559b1c3d50d46012
Alex Kashuba <o.kashuba@gmail.com>
2019-01-31 21:12:58 +0100

    Manifest for non .py files

--- setup.py
+++ setup.py
@@ -8,12 +8,21 @@ from setuptools import setup
 setup(name='lexicamaker',
   version='0.1.2',
   description='DSL to Apple Dictionary Service converter',
+  long_description='The program takes DICT.dsl file, looks for abbreviations DICT_abrv.dsl and annotation DICT.ann and produces the a folder for Apple Dictionary Services, which has to be compiled with Dictinary Developer Kit to get the DICT.dictionary package, which one can install in ~/Library/Dictionaries/',
+  classifiers=[
+                   'Development Status :: 2 - Pre-Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.7',
+                   'Topic :: Text Processing :: Linguistic',
+                   ],
+  keywords='dsl apple dictionary'
   url='http://github.com/lnxk/adsmaker',
-  author='Svintuss; Lnxk',
-  author_email='svintuss@gmail.com',
+  author='Lnxk; Svintuss',
+  author_email='alnika@mail.ru; svintuss@gmail.com',
   license='MIT',
   packages=['lexicamaker'],
   zip_safe=True,
+  include_package_data=True
   entry_points = {
     'console_scripts': ['adsmaker=lexicamaker.__main__:main'],
   }
