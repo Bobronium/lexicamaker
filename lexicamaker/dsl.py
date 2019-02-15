77fbde6df9bbb17fa7731ee69a0627d3f6dd266c
Alex Kashuba <o.kashuba@gmail.com>
2019-02-16 00:12:11 +0100

    Expanded the entry building features
    + Test for processDSLentry written

--- lexicamaker/dsl.py
+++ lexicamaker/dsl.py
@@ -4,32 +4,48 @@
 import re
 
 def processDSLbodyline(myString):
-    
+    """ Processing the line by substitution of every known tag by xml tag and wrapping free parts as paragraphs. """
     theString = myString
     DSLXMLdictionary = {
                         r'\[m\]'    : '<div>',
                         r'\[m(?P<indent>[0-9])\]': r'<div class="m\g<indent>">',
                         r'\[/m\]'   : '</div>',
+                        r'\[br\]'    : '<br />',
+                        r'\[\*\]'   : '<span d:priority="2">',
+                        r'\[/\*\]'  : '</span>',
                         r'\[b\]'    : '<b>', r'\[/b\]': '</b>',
                         r'\[i\]'    : '<i>', r'\[/i\]': '</i>',
                         r'\[u\]'    : '<u>', r'\[/u\]': '</u>',
-                        r'\[c\]'    : '<font color="green">',
+                        r'\[c\]'                   : '<font color="green">',
                         r'\[c (?P<color>[a-z]+)\]' : r'<font color="\g<color>">',
-                        r'\[/c\]'   : '</font>',
+                        r'\[/c\]'                  : r'</font>',
                         r'\[sub\]'  : '<sub>', r'\[/sub\]': '</sub>',
                         r'\[sup\]'  : '<sup>', r'\[/sup\]': '</sup>',
                         r'\[\'\]'   : '', r'\[/\'\]'    : '\u0301',
-                        r'\[p\]'    : '', r'\[/p\]'     : '',
+                        r'\[lang\]'                          : '',
+                        r'\[lang name="(?P<lname>[a-z]+)"\]' : '',
+                        r'\[lang id="(?P<lid>[a-z]+)"\]'     : '',
+                        r'\[/lang\]'                         : '',
                         r'\[trn\]'  : '', r'\[/trn\]'   : '',
                         r'\[ex\]'   : '', r'\[/ex\]'    : '',
                         r'\[com\]'  : '', r'\[/com\]'   : '',
-                        r'\[t\]'    : '', r'\[/t\]'     : '',
+                        r'\[!trs\]' : '', r'\[/!trs]'   : '',
+                        r'\[p\]'    : '', r'\[/p\]'     : '',
+                        r'\[t\]'    : '<span d:pr="1">',
+                        r'\[/t\]'   : '</span>',
                         r'\[s\]'    : '', r'\[/s\]'     : '',
-                        r'\[!trs\]' : '', r'\[/!trs]'   : ''
+                        r'<<'       : '', r'>>'         : '',
+                        r'\[ref\]'  : '', r'\[/ref\]'   : '',
+                        r'\[url\]'  : '', r'\[/url\]'   : ''
                     }
+
+    # Simply substitute every tag in the line
     for tag in DSLXMLdictionary:
         myString = re.sub(tag, DSLXMLdictionary[tag], myString)
 
+    # Make sure that the line is treated as a paragraph. Either the whole line is in <div>,
+    # or if there is any new paragraph <div>s in the line, wrap the free beginning and ending
+    # of the line in <div>s
     myStringSplit = re.split(r'(<div(?: [^<>]+)*>|</div>)',myString)
     if len(myStringSplit) == 1:
         myString = '<div>' + myString + '</div>'
@@ -38,8 +54,65 @@ def processDSLbodyline(myString):
             if myStringSplit[i]:
                 myStringSplit[i] = '<div>' + myStringSplit[i] + '</div>'
         myString = ''.join(myStringSplit)
+
+    # return resulting line
     return myString
 
+def processDSLentryhead(entryhead):
+    head = ''
+    for line in entryhead:
+        head = head + '<d:index d:value="' + line + '" d:title="' + line + '"/>\n'
+    id = re.sub('[^a-z0-9]', '_', entryhead[0].lower())
+    return [id, head]
+
+def processDSLentrybody(entrybody):
+    body = ''
+    for line in entrybody:
+        body = body + processDSLbodyline(line) + '\n'
+    return body
+
+def processDSLentry(entryhead, entrybody):
+    id, head = processDSLentryhead(entryhead)
+    body = processDSLentrybody(entrybody)
+    entry = '<d:entry id="' + id + '">\n' + head + body + '</d:entry>\n'
+    return [id, entry]
+
+def processDSLfile(dslFile):
+    entryhead = []
+    entrybody = []
+    isSameEntry = None
+    for line in dslFile:
+        # remove newline sequence in the end of the string
+        line = line.rstrip()
+        # remove comments
+        line = re.sub('{{.*?}}', '', line)
+        # skip empty lines
+        if re.fullmatch(r'\s*', line):
+            pass
+        # skip lines starting from hash # symbol
+        elif re.match(r'#', line):
+            pass
+        # process the lines starting from any non-empty symbol and treat them as headwords
+        elif re.match(r'[^\s]', line):
+            if isSameEntry:
+                entryhead.append(line)
+            else:
+                if isSameEntry == False:
+                    __write_entry__(processDSLentry(entryhead, entrybody))
+                entryhead = [line]
+                entrybody = []
+                isSameEntry = True
+        # prcess the lines starting from any number of empty symbols and treat them as entry body
+        elif re.match(r'\s', line):
+            entrybody.append(re.sub(r'\s+', '', line, 1))
+            isSameEntry = False
+    __write_entry__(processDSLentry(entryhead, entrybody))
+
+def __write_entry__(string):
+    print('========================')
+    print(string[1])
+                             
+                             
 #def __parse_c__(myString):
 #    if myString == None:
 #        return "<font color=\"green\">"
@@ -55,3 +128,8 @@ def processDSLbodyline(myString):
 #myString = processDSLbodyline(theDSLbodyline)
 
 #print(myString)
+
+#dslFile = open('../tests/test_convert_data/3WordsDictionary.dsl', 'r', encoding='utf-16')
+#processDSLfile(dslFile)
+                             
+
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -4,7 +4,7 @@
 import os
 
 from .context import lexicamaker
-from lexicamaker.dsl import processDSLbodyline
+from lexicamaker.dsl import *
 
 
 def setup_function(function):
@@ -29,4 +29,35 @@ def test_convert_line_paragraps():
     
     assert processDSLbodyline(strIn1) == strOut1
 
+def test_convert_line_star():
+    """Checks conversion of the simplest tags."""
+    
+    strIn1  = r"[m1]\[[u]'əup(ə)n[/u]\][/m] [m2] [*][c]брит.[/c] [b]1.[/b] [i]прил.[/i][/*] открытый"
+    strOut1 = "<div class=\"m1\">\\[<u>'əup(ə)n</u>\\]</div> <div class=\"m2\"><div> <span d:priority=\"2\"><font color=\"green\">брит.</font> <b>1.</b> <i>прил.</i></span> открытый</div>"
+    
+    assert processDSLbodyline(strIn1) == strOut1
 
+
+def test_convert_entry():
+    inStr1 = r"""please, do not abandon me!
+abandonee
+"""
+    inStr2 = r"""\[[t]əˌbændə'niː[/t]\]
+[p]сущ.[/p]
+[m1]1) [p][trn]юр.[/p] лицо, в пользу которого имеет место отказ от права[/trn][/m]
+[m1]2) [p][trn]мор.[/p] страховщик, в пользу которого остаётся застрахованный груз [i]или[/i] застрахованное судно в случае аварии[/trn][/m]
+"""
+    outStr0 = r"""<d:entry id="please__do_not_abandon_me_">
+<d:index d:value="please, do not abandon me!" d:title="please, do not abandon me!"/>
+<d:index d:value="abandonee" d:title="abandonee"/>
+<div>\[<span d:pr="1">əˌbændə'niː</span>\]</div>
+<div>сущ.</div>
+<div class="m1">1) юр. лицо, в пользу которого имеет место отказ от права</div>
+<div class="m1">2) мор. страховщик, в пользу которого остаётся застрахованный груз <i>или</i> застрахованное судно в случае аварии</div>
+</d:entry>
+"""
+    outStr00 = 'please__do_not_abandon_me_'
+    id, entry = processDSLentry(inStr1.splitlines(False), inStr2.splitlines(False))
+    
+    assert id == outStr00
+    assert entry == outStr0
