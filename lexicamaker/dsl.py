30452441f78c8925a3f7648b0c9134726c4e0f23
Alex Kashuba <o.kashuba@gmail.com>
2019-02-21 10:57:21 +0100

    Preparations to pass data through attributes

--- lexicamaker/dsl.py
+++ lexicamaker/dsl.py
@@ -3,13 +3,19 @@
 
 import re
 
-def processDSLbodyline(myString):
-    """ Processing the line by substitution of every known DSL tag by XML tag and wrapping free parts as paragraphs by <div>s. """
-    theString = myString
+
+
+
+def processDSLstring(string):
+    """ Processing the string by substitution of every known DSL tag by XML tag or calling the corresponding __parse_tag__ function """
+
     DSLtoXMLmaping = {
-                        # here it's better to match [m] tags separately, while closing tag [/m] can be in the different line
+                        # TYPE I TAGS: text-independent, one can map open and closing tags separately.
+                        # It is useful if opening and closing tags are not in the same string.
+                        # For example: here it's better to match [m] tags separately, while closing tag [/m] can be in the different line
                         r'\[m\]'                  : r'<div>',
-                        r'\[m(?P<indent>[0-9])\]' : r'<div class="m\g<indent>">',
+                        # here \d stays for decimal digit, it's [0-9] and in case of Unicode other digit characters
+                        r'\[m(?P<indent>\d)\]' : r'<div class="m\g<indent>">',
                         r'\[/m\]'                 : r'</div>',
                         r'\[br\]'   : r'<br />',
                         r'\[\*\]'   : r'<span d:priority="2">',
@@ -17,55 +23,103 @@ def processDSLbodyline(myString):
                         r'\[b\]'    : r'<b>', r'\[/b\]'  : r'</b>',
                         r'\[i\]'    : r'<i>', r'\[/i\]'  : r'</i>',
                         r'\[u\]'    : r'<u>', r'\[/u\]'  : r'</u>',
-                        r'\[c\]'                   : r'<font color="green">',
-                        r'\[c (?P<color>[a-z]+)\]' : r'<font color="\g<color>">',
-                        r'\[/c\]'                  : r'</font>',
+                        # here \w stays for word characters, it's [a-zA-Z0-9_] and in case of Unicode most characters that can be part of a word in any language
+                        r'\[c\]'                : r'<font color="green">',
+                        r'\[c (?P<color>\w+)\]' : r'<font color="\g<color>">',
+                        r'\[/c\]'               : r'</font>',
                         r'\[sub\]'  : r'<sub>', r'\[/sub\]'  : '</sub>',
                         r'\[sup\]'  : r'<sup>', r'\[/sup\]'  : '</sup>',
                         r'\[\'\]'   : '', r'\[/\'\]'    : '\u0301',
-                        r'\[lang\]'                             : '',
-                        r'\[lang name="(?P<lname>[a-zA-Z]+)"\]' : '',
-                        r'\[lang id=(?P<lid>[0-9]+)\]'          : '',
-                        r'\[/lang\]'                            : '',
+                        
+                        # TYPE II TAGS: call a function instead of the substitution pattern, can process text between tags.
+                        # Downside is that opening and closing tags should be in the same line.
+                        # In case they isn't one has to substitute free dangling tags by empty string afterwards
+                        
+                        
+                        r'\[ex\](?P<text>.*?)\[/ex\]'   : __parse_ex__,
+                        r'\[lang\](?P<text>.*?)\[/lang\]'                      : __parse_lang__,
+                        r'\[lang name="(?P<name>\w+)"\](?P<text>.*?)\[/lang\]' : __parse_lang__,
+                        r'\[lang id=(?P<id>\d+)\](?P<text>.*?)\[/lang\]'       : __parse_lang__,
+                        
                         r'\[trn\]'  : '', r'\[/trn\]'   : '',
-                        r'\[ex\](?P<text>.*?)\[/ex\]'   : '', #__parse_ex__,
                         r'\[com\]'  : '', r'\[/com\]'   : '',
                         r'\[!trs\]' : '', r'\[/!trs]'   : '',
+                        
+                        r'\[lang\]'            : '',
+                        r'\[lang name="\w+"\]' : '',
+                        r'\[lang id=\d+\]'     : '',
+                        r'\[/lang\]'           : '',
+                        
                         r'\[p\]'    : '', r'\[/p\]'     : '',
                         r'\[t\]'    : r'<span d:pr="1">',
                         r'\[/t\]'   : r'</span>',
                         r'\[s\]'    : '', r'\[/s\]'     : '',
                         r'<<'       : '', r'>>'         : '',
                         r'\[ref\]'                             : '',
-                        r'\[ref dict="(?P<dname>[a-zA-Z]+)"\]' : '',
+                        r'\[ref dict="(?P<dname>[\w\s]+)"\]' : '',
                         r'\[/ref\]'                            : '',
                         r'\[url\]'  : '', r'\[/url\]'   : ''
                     }
 
     # Simply substitute every tag in the line
     for tag in DSLtoXMLmaping:
-        myString = re.sub(tag, DSLtoXMLmaping[tag], myString)
+        string = re.sub(tag, DSLtoXMLmaping[tag], string)
 
+    return string
+
+def __parse_ex__(match):
+    print(match)
+    print(match.group())
+    print(match.expand('\g<text>'))
+    
+    processDSLstring.__indexing__ = True
+    
+    # run indexing and processing
+
+    del(processDSLstring.__indexing__)
+    
+    return match.expand('\g<text>')
+
+def __parse_lang__(match):
+    print(match)
+    print(match.group())
+    print(match.expand('\g<text>'))
+    
+    processDSLstring.__language__ = match.expand('\g<name>')
+    
+    # run indexing and processing
+    
+    del(processDSLstring.__language__)
+    
+    return match.expand('\g<text>')
+
+def processDSLbodyline(string):
+    """ Processing the line by calling processDSLstring() and wrapping free parts as paragraphs by <div>s. """
+
+    string = processDSLstring(string)
+    
     # Make sure that a line is treated as a paragraph.
     # Either the whole line is wrapped in <div>,
     # or if there is any new paragraph <div>s in the line,
     # wrap the free beginning and ending of the line in <div>s
 
-    myStringSplit = re.split(r'(<div(?: [^<>]+)*>|</div>)',myString)
-    if len(myStringSplit) == 1:
-        myString = '<div>' + myString + '</div>'
+    stringSplit = re.split(r'(<div(?: [^<>]+)*>|</div>)',string)
+    if len(stringSplit) == 1:
+        string = '<div>' + string + '</div>'
     else:
         for i in [0,-1]:
-            if myStringSplit[i]:
-                myStringSplit[i] = '<div>' + myStringSplit[i] + '</div>'
-    #for i, subString in list(enumerate(myStringSplit))[0::2]:
-    #    myStringSplit[i] = '<div>' + subString + '</div>'
+            if stringSplit[i]:
+                stringSplit[i] = '<div>' + stringSplit[i] + '</div>'
+    #for i, subString in list(enumerate(stringSplit))[0::2]:
+    #    stringSplit[i] = '<div>' + subString + '</div>'
 
-    myString = ''.join(myStringSplit)
-    
+    string = ''.join(stringSplit)
 
     # return resulting line
-    return myString
+    return string
+
+
+
 
 def processDSLentryhead(entryhead):
     """ This function generates the directives <d:index d:value="" d:title=""/> for further indexing. It also returns the id of the whole entry generated form the first headword """
@@ -97,14 +151,14 @@ def processDSLfile(dslFile, xmlFile):
         line = line.rstrip()
         # remove comments
         line = re.sub('{{.*?}}', '', line)
-        # skip empty lines
+        # skip empty lines, \s stays for whitespace characters [ \t\n\r\f\v], and in case of Unicode also many other characters, for example the non-breaking spaces mandated by typography rules in many languages
         if re.fullmatch(r'\s*', line):
             pass
         # skip lines starting from hash # symbol
         elif re.match(r'#', line):
             pass
-        # process the lines starting from any non-empty symbol and treat them as headwords
-        elif re.match(r'[^\s]', line):
+        # process the lines starting from any non-empty symbol and treat them as headwords, \S is not a whitespace character which is the opposite of \s
+        elif re.match(r'\S', line):
             if isSameEntry:
                 entryhead.append(line)
             else:
@@ -143,15 +197,24 @@ def writeBaseTags(xmlFile, opening = True):
 
 #theDSLbodyline = r"[c]брит.[/c]"
 
-theDSLbodyline = "[m1]1) [i][trn][com]способ изготовления изображений[/com][/trn][/i][/m][m2][*][ex][lang id=1033]photography[/lang] — фотография[/ex][/*][/m][m2][*][ex][lang id=1033]radiography[/lang] — радиография[/ex][/*][/m]"
+theDSLbodyline = "[c blue]брит.[/c] [m1]1) [i][trn][com]способ изготовления изображений[/com][/trn][/i][/m][m2][*][ex][lang name=\"English\"]photography[/lang] — фотография[/ex][/*][/m][m2][*][lang name=\"English\"]radiography[/lang] — [ex]радиография[/ex][/*][/m]"
 
-#myString = processDSLbodyline(theDSLbodyline)
+myString = processDSLbodyline(theDSLbodyline)
 
-result = re.findall(r'\[lang id=(?P<lid>[0-9]{4})\](?P<text>.*?)\[/lang\]', theDSLbodyline)
+#result = re.findall(r'\[lang id=(?P<lid>[0-9]{4})\](?P<text>.*?)\[/lang\]', theDSLbodyline)
 
-print(result)
+print(myString)
 
 
+
+#def __f__(m):
+#    number_mapping = {'1': 'one', '2': 'two', '3': 'three'}
+#    print(m)
+#    print(m.group())
+#    print(m.expand())
+#    return number_mapping[m.group()]
+
+#print(re.sub(r'\d', __f__, "1 testing 2 3"))
 
 #import xml.etree.ElementTree as ET
 
